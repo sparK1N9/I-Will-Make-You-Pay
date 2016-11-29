d8f9069753203ecad54067bd757a37b76afbedaf
sparK1N9 <spacecqcenhancer@gmail.com>
2016-11-29 15:28:05 -0500

    scripts for the ui

---
+++ I Will Make You Pay/Assets/scripts/p1info.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class p1info : MonoBehaviour {
+    public string text;
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        text = "$" + player1.main.funds.ToString("N2");
+        text += "HP: " + player1.main.currentHealth;
+        GetComponent<Text>().text = text;
+    }
+}
---
+++ I Will Make You Pay/Assets/scripts/p2info.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class p2info : MonoBehaviour {
+    public string text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        text = "$" + player2.main.funds.ToString("N2");
+        text += "HP: " + player2.main.currentHealth;
+        GetComponent<Text>().text = text;
+    }
+}
