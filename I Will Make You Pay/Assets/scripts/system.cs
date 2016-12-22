52f614b98d71498cbac0bae655c099ef24f7a797
sparK1N9 <spacecqcenhancer@gmail.com>
2016-12-22 15:48:35 -0500

    final 1.00

--- I Will Make You Pay/Assets/scripts/system.cs
+++ I Will Make You Pay/Assets/scripts/system.cs
@@ -27,6 +27,15 @@ public class system : MonoBehaviour
     public bool up1 = false;
     public bool up2 = false;
     public bool bu = false;
+    int y = 0;
+    public RawImage a1;
+    public RawImage a2;
+    public RawImage a3;
+    public RawImage a4;
+    public RawImage a5;
+    public RawImage a6;
+    public RawImage a7;
+    public RawImage a8;
 
     void Awake()
     {
@@ -47,37 +56,62 @@ public class system : MonoBehaviour
         tx = GetComponentsInChildren<Text>();
         tx[0].enabled = false;
         ui.enabled = false;
+        a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
     }
 
     void Update()
     {
-        if (up2)
+        if (Input.GetKeyDown(KeyCode.Space) && y == 0)
         {
-            trans2.Play();
-            score1++;
-            funds2 += 2000;
-            funds1 += 5000;
-            StartCoroutine(HitPause(1.0f));
-            SceneManager.LoadScene("game");
-            up2 = false;
+            y++;
+            a1.enabled = true;
         }
-        else if (up1)
+        else if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && y == 1)
         {
-            trans.Play();
-            score2++;
-            funds1 += 2000;
-            funds2 += 5000;
-            StartCoroutine(HitPause(1.0f));
-            SceneManager.LoadScene("game");
-            up1 = false;
+            a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
+            y++;
+            a2.enabled = true;
         }
-        if (bu)
+        else if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && y == 2)
         {
-            bu = false;
-            bui.Play();
+            a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
+            y++;
+            a3.enabled = true;
         }
-        if (Input.GetKeyDown(KeyCode.Space) && i == 0)
+        else if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && y == 3)
         {
+            a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
+            y++;
+            a4.enabled = true;
+        }
+        else if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && y == 4)
+        {
+            a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
+            y++;
+            a5.enabled = true;
+        }
+        else if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && y == 5)
+        {
+            a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
+            y++;
+            a6.enabled = true;
+        }
+        else if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && y == 6)
+        {
+            a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
+            y++;
+            a7.enabled = true;
+        }
+        else if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && y == 7)
+        {
+            a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
+            y++;
+            a8.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && y == 8)
+        {
+            y++;
+            a1.enabled = a2.enabled = a3.enabled = a4.enabled = a5.enabled = a6.enabled = a7.enabled = a8.enabled = false;
             i++;
             bgm.Stop();
             sfxSource.Play();
@@ -86,40 +120,69 @@ public class system : MonoBehaviour
             st.enabled = false;
             ui.enabled = true;
         }
-        if ((score1 == 5 || funds1 <= 0) && i != 2)
+        else if (y == 9)
         {
-            win1.Play();
-            tx[0].enabled = false;
-            ui.enabled = false;
-            SceneManager.LoadScene("start", LoadSceneMode.Single);
-            p1w.enabled = true;
-            i = 2;
-        }
-        else if ((score2 == 5 || funds2 <= 0) && i != 2)
-        {
-            win2.Play();
-            tx[0].enabled = false;
-            ui.enabled = false;
-            SceneManager.LoadScene("start");
-            p2w.enabled = true;
-            i = 2;
-        }
-        if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && i == 2)
-        {
-            p2w.enabled = p1w.enabled = false;
-            st.enabled = true;
-            bgm.Play();
-            i = 0;
-            score1 = score2 = 0;
-            funds1 = funds2 = 9001;
-            tx[0].text = "";
-            SceneManager.LoadScene("start");
-        }
-        if (i != 0)
-        {
-            text = score2.ToString("N0") + "\t\t\t\t\t\t\t";
-            text += score1.ToString("N0");
-            tx[0].text = text;
+            if (up2)
+            {
+                trans2.Play();
+                score1++;
+                funds2 += 2000;
+                funds1 += 5000;
+                StartCoroutine(HitPause(1.0f));
+                SceneManager.LoadScene("game");
+                up2 = false;
+            }
+            else if (up1)
+            {
+                trans.Play();
+                score2++;
+                funds1 += 2000;
+                funds2 += 5000;
+                StartCoroutine(HitPause(1.0f));
+                SceneManager.LoadScene("game");
+                up1 = false;
+            }
+            if (bu)
+            {
+                bu = false;
+                bui.Play();
+            }
+            if ((score1 == 5 || funds1 <= 0) && i != 2)
+            {
+                win1.Play();
+                tx[0].enabled = false;
+                ui.enabled = false;
+                SceneManager.LoadScene("start", LoadSceneMode.Single);
+                p1w.enabled = true;
+                i = 2;
+            }
+            else if ((score2 == 5 || funds2 <= 0) && i != 2)
+            {
+                win2.Play();
+                tx[0].enabled = false;
+                ui.enabled = false;
+                SceneManager.LoadScene("start");
+                p2w.enabled = true;
+                i = 2;
+            }
+            if ((Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) && i == 2)
+            {
+                p2w.enabled = p1w.enabled = false;
+                st.enabled = true;
+                bgm.Play();
+                i = 0;
+                score1 = score2 = 0;
+                funds1 = funds2 = 9001;
+                tx[0].text = "";
+                SceneManager.LoadScene("start");
+                y = 0;
+            }
+            if (i != 0)
+            {
+                text = score2.ToString("N0") + "\t\t\t\t\t\t\t";
+                text += score1.ToString("N0");
+                tx[0].text = text;
+            }
         }
     }
 
